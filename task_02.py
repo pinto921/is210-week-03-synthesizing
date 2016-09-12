08f020de84fb4768fb66493b4651b0b9ce4b4df5
pinto921 <pinto921@hotmail.com>
2016-09-12 06:42:24 +0000

    Task 2

--- task_02.py
+++ task_02.py
@@ -4,3 +4,19 @@
 
 
 import inquisition
+
+FLEMISH = inquisition.SPANISH
+
+Random_word = 'Spanish'
+
+length_word = len(Random_word)
+
+finding = inquisition.SPANISH.index('Spanish')
+
+Variable_1 = FLEMISH[:finding]
+
+Variable_2 = FLEMISH[finding + length_word:]
+
+FLEMISH = Variable_1 + 'Flemish' + Variable_2
+
+print FLEMISH
